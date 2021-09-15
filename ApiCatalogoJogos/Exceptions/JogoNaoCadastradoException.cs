4d4e2d66525d8d7e2009b4e14a4a5aa55c69747e
Viegas <milton.viegas.br@gmail.com>
2021-09-14 22:59:13 -0300

    first commit

---
+++ ApiCatalogoJogos/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Exceptions
+{
+    public class JogoNaoCadastradoException: Exception
+    {
+        public JogoNaoCadastradoException()
+            :base("Este jogo não está cadastrado")
+        {}
+    }
+}
+
+
+
+
